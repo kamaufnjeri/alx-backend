127e95448f293aef4e439135f0ad6db825724e7b
kamaufnjeri <kamaufnjeri2019@gmail.com>
2024-01-31 14:40:01 +0300

    put using lifo cache

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""fifo caching method"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    class FIFOCache inherits from BaseCache
+    """
+    def __init__(self):
+        super().__init__()
+        self.order_cache = []
+
+    def put(self, key, item):
+        """add to cached data"""
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discard_key = self.order_cache[len(self.order_cache) - 1]
+                self.order_cache.remove(discard_key)
+                del self.cache_data[discard_key]
+                print(f"DISCARD: {discard_key}")
+            self.cache_data[key] = item
+            self.order_cache.append(key)
+
+    def get(self, key):
+        """get by key"""
+        if key is not None and key in self.cache_data.keys():
+            self.order_cache.remove(key)
+            self.order_cache.append(key)
+            return self.cache_data.get(key)
+        else:
+            return None
