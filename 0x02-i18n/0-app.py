42d5df4bbc93a02c71c3743e05ab53f1fb269750
kamaufnjeri <kamaufnjeri2019@gmail.com>
2024-02-07 03:31:41 +0300

    creating a flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Basic flask app"""
 from flask import Flask, render_template
 
 my_app = Flask(__name__)
@@ -6,6 +7,7 @@ my_app = Flask(__name__)
 
 @my_app.route('/', strict_slashes=False)
 def display_index() -> str:
+    """run the app html"""
     return render_template('0-index.html')
 
 
